7b0f724a3cb3fb9b0fbb36465c560f87dbd1705d
Kingkhrish <lkkhrish@gmail.com>
2023-08-31 21:40:59 -0700

    Coin Game 3rd Push

--- HomeScreen.py
+++ HomeScreen.py
@@ -9,12 +9,18 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption("Coin Flipper")
 BLUE = (137, 207, 240)
+white=(255,255,255)
+
 FPS = 60
 
 flip_once_img = pygame.image.load('Assets/flip_once_button.png').convert_alpha()
 flip_five_img = pygame.image.load('Assets/flip_five_times_button.png').convert_alpha()
 heads_coin_img = pygame.image.load('Assets/Heads-removebg-preview.png').convert_alpha()
 tails_coin_img = pygame.image.load('Assets/Tails-removebg-preview.png').convert_alpha()
+
+font = pygame.font.SysFont("Arial", 36)
+
+
 
 show_heads = False
 show_tails = False
@@ -54,77 +60,91 @@ class Button():
 once_button = Button(50, 400, flip_once_img, 2)
 five_button = Button(300, 400, flip_five_img, 2)
 
+def count_heads():
+    global heads
+    heads += 1
+    txtheads = font.render("Heads: " + str(heads), True, white)
+    screen.blit(txtheads, (10, 20))
+
+def count_tails():
+    global tails
+    tails += 1
+    txttails = font.render("Tails: " + str(tails), True, white)
+    screen.blit(txttails, (250, 20))
+def decide_coin():
+
+        rando = random.randint(0, 1)
+        global coin_bias
+        if rando == 0:
+            coin_bias = True
+        else:
+            coin_bias = False
+        print(coin_bias)
+
+def reset_counter():
+    global heads, tails
+
+    heads = 0
+    tails = 0
 
 def draw_window():
     if once_button.draw():
-        unbiased_flip_once()
+        if coin_bias:
+            biased_flip_once()
+        else:
+            unbiased_flip_once()
     if five_button.draw():
-        unbiased_flip_five()
+        if coin_bias:
+            biased_flip_five()
+        else:
+            unbiased_flip_five()
     pygame.display.update()
 
-
 def biased_flip_once():
-    recordList = []
-    heads = 0
-    tails = 0
+    global tails, heads
     for i in range(1):
         flip = random.random()
         if flip < 0.65:
-            print("Heads")
-            recordList.append("Heads")
-            heads += 1
+            screen.blit(heads_coin_img, (200, 200))
+            count_heads()
         else:
-            print("Tails")
-            recordList.append("Tails")
-            tails += 1
+            screen.blit(tails_coin_img, (200, 200))
+            count_tails()
 
 def biased_flip_five():
-    recordList = []
-    heads = 0
-    tails = 0
+    global tails, heads
     for i in range(5):
         flip = random.random()
         if flip < 0.65:
-            print("Heads")
-            recordList.append("Heads")
-            heads += 1
+            screen.blit(heads_coin_img, (200, 200))
+            count_heads()
+            print(heads)
         else:
-            print("Tails")
-            recordList.append("Tails")
-            tails += 1
+            screen.blit(tails_coin_img, (200, 200))
+            count_tails()
 
 def unbiased_flip_once():
-    recordList = []
-    heads = 0
-    tails = 0
+    global tails, heads
     for i in range(1):
         flip = random.randint(0, 1)
         if flip == 0:
             screen.blit(heads_coin_img, (200, 200))
-            recordList.append("Heads")
-            heads += 1
+            count_heads()
         else:
             screen.blit(tails_coin_img, (200, 200))
-            recordList.append("Tails")
-            tails += 1
-    print(str(recordList))
+            count_tails()
 
 
 def unbiased_flip_five():
-    recordList = []
-    heads = 0
-    tails = 0
+    global tails, heads
     for i in range(5):
         flip = random.randint(0, 1)
         if flip == 0:
             screen.blit(heads_coin_img, (200, 200))
-            recordList.append("Heads")
-            heads += 1
+            count_heads()
         else:
             screen.blit(tails_coin_img, (200, 200))
-            recordList.append("Tails")
-            tails += 1
-    print(str(recordList))
+            count_tails()
 
 
 def main():
@@ -139,6 +159,9 @@ def main():
         draw_window()
     pygame.quit()
 
-
 if __name__ == "__main__":
+    decide_coin()
+    reset_counter()
     main()
+
+
